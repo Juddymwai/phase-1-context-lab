84e83a7ef3882defd14da2d0e992e51ed7a2df48
Judy Mwai <juddiemukami@gmail.com>
2022-06-23 23:35:17 +0300

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,69 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ let createEmployeeRecord = (array) => {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title:array[2],
+      payPerHour:array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+ const createEmployeeRecords = (arrays) => {
+    
+    return arrays.map(array => {
+        return createEmployeeRecord(array)
+    })
+
+}
+let createTimeInEvent = (employeeRecord, dateStamp) => {
+    const [date, hour] = dateStamp.split(" ")
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+  
+    })
+    return employeeRecord
+  }
+  const createTimeOutEvent = (employeeRecord, dateStamp) => {
+    const [date, hour] = dateStamp.split(" ")
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+  
+  
+    })
+    return employeeRecord
+  }
+
+
+  const hoursWorkedOnDate = (employeeRecord, dateStamp) => {
+    const timeIn = employeeRecord.timeInEvents.find((event) => {
+      return event.date === dateStamp
+  
+    })
+  
+    const timeOut = employeeRecord.timeOutEvents.find((event) => {
+      return event.date === dateStamp
+    })
+  
+    const timeWorked = (timeOut.hour - timeIn.hour) / 100
+    return timeWorked
+    
+  }
+
+  const wagesEarnedOnDate = (employeeRecord, dateStamp) => {
+    const Earned = hoursWorkedOnDate (employeeRecord, dateStamp)
+    const Wage = Earned * employeeRecord.payPerHour
+    return parseInt(currentWage)
+  
+  }
+  
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,5 +82,16 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+
 }
+
+const calculatePayroll = (employeeArray) => {
+    return employeeArray.reduce((total, record) => {
+      return total + allWagesFor(record)
+    }, 0)
+  
+  }
+
+
+
 
